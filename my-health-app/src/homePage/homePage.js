ed8f44099928c7dee7c4a8c5466dad3c41352185
Sidharth Sachdev <sid.sachdev@mail.utoronto.ca>
2025-03-21 12:55:44 -0400

    Connected the story aspect to the react app by inludcing everything in the public folder of the react app.

--- my-health-app/src/homePage/homePage.js
+++ my-health-app/src/homePage/homePage.js
@@ -11,16 +11,16 @@ function HomePage() {
     navigate("/vitals");
   };
 
-  // const handleHealth = () => {
-  //   navigate("/health");
-  // }
+  const handleHealth = () => {
+    window.location.href = "/index-layout.html";
+  };
 
   return (
     <div className="split-container">
       <div className="split left" onClick={handleVitals}>
         <h1>Visualize Your Vitals</h1>
       </div>
-      <div className="split right">
+      <div className="split right" onClick={handleHealth}>
         <h1>Visualize General Health Trends</h1>
       </div>
     </div>
