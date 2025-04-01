58b78f5dcc16b1f3ca8a08f1eb7d34196e50acd3
Sidharth Sachdev <sid.sachdev@mail.utoronto.ca>
2025-03-31 23:46:37 -0400

    Moved from react to normal index.html. Made all vizualizations have dynamic width and height.

--- js/LineChart.js
+++ js/LineChart.js
@@ -4,7 +4,7 @@ class LineChart {
       parentElement: _config.parentElement,
       width: _config.width || 700,
       height: _config.height || 400,
-      margin: _config.margin || { top: 30, right: 30, bottom: 50, left: 60 }
+      margin: _config.margin || { top: 30, right: 30, bottom: 50, left: 60 },
     };
     this.data = _config.data;
     this.annotations = _config.annotations;
@@ -14,44 +14,58 @@ class LineChart {
   initVis() {
     const vis = this;
 
-    vis.width = vis.config.width - vis.config.margin.left - vis.config.margin.right;
-    vis.height = vis.config.height - vis.config.margin.top - vis.config.margin.bottom;
+    vis.width =
+      document.getElementById(vis.config.parentElement).getBoundingClientRect()
+        .width -
+      vis.config.margin.left -
+      vis.config.margin.right;
+    vis.height =
+      document.getElementById(vis.config.parentElement).getBoundingClientRect()
+        .height -
+      vis.config.margin.top -
+      vis.config.margin.bottom;
 
     // create svg
-    vis.svg = d3.select(vis.config.parentElement)
+    vis.svg = d3
+      .select("#" + vis.config.parentElement)
       .append("svg")
       .attr("width", vis.config.width)
       .attr("height", vis.config.height)
       .append("g")
-      .attr("transform", `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+      .attr(
+        "transform",
+        `translate(${vis.config.margin.left},${vis.config.margin.top})`
+      );
 
     // init scale and line generator
-    vis.xScale = d3.scaleLinear()
-      .range([0, vis.width]);
+    vis.xScale = d3.scaleLinear().range([0, vis.width]);
 
-    vis.yScale = d3.scaleLinear()
-      .range([vis.height, 0]);
+    vis.yScale = d3.scaleLinear().range([vis.height, 0]);
 
-    vis.lineGenerator = d3.line()
-      .x(d => vis.xScale(d.month))
-      .y(d => vis.yScale(d.cortisol))
+    vis.lineGenerator = d3
+      .line()
+      .x((d) => vis.xScale(d.month))
+      .y((d) => vis.yScale(d.cortisol))
       .curve(d3.curveMonotoneX);
 
     // add axis groups
-    vis.xAxisG = vis.svg.append("g")
+    vis.xAxisG = vis.svg
+      .append("g")
       .attr("transform", `translate(0, ${vis.height})`);
 
     vis.yAxisG = vis.svg.append("g");
 
     // add labels
-    vis.svg.append("text")
+    vis.svg
+      .append("text")
       .attr("class", "x-axis-label")
       .attr("x", vis.width / 2)
       .attr("y", vis.height + 35)
       .attr("text-anchor", "middle")
       .text("Months from Today");
 
-    vis.svg.append("text")
+    vis.svg
+      .append("text")
       .attr("class", "y-axis-label")
       .attr("transform", "rotate(-90)")
       .attr("y", -30)
@@ -60,7 +74,8 @@ class LineChart {
       .text("Average Cortisol Level (ng/dL)");
 
     // add title
-    vis.svg.append("text")
+    vis.svg
+      .append("text")
       .attr("class", "title")
       .attr("x", vis.width / 2)
       .attr("y", -15)
@@ -73,9 +88,9 @@ class LineChart {
   wrangleData() {
     let vis = this;
 
-    vis.workingData = vis.data.filter(d => d.activity === "working");
-    vis.restData = vis.data.filter(d => d.activity === "resting");
-    vis.meditatingData = vis.data.filter(d => d.activity === "meditating");
+    vis.workingData = vis.data.filter((d) => d.activity === "working");
+    vis.restData = vis.data.filter((d) => d.activity === "resting");
+    vis.meditatingData = vis.data.filter((d) => d.activity === "meditating");
 
     vis.updateVis();
   }
@@ -84,140 +99,153 @@ class LineChart {
     const vis = this;
 
     // update scales and axes
-    vis.xScale.domain(d3.extent(vis.workingData, d => d.month));
-    vis.yScale.domain([0, d3.max(vis.workingData, d => d.cortisol)]).nice();
+    vis.xScale.domain(d3.extent(vis.workingData, (d) => d.month));
+    vis.yScale.domain([0, d3.max(vis.workingData, (d) => d.cortisol)]).nice();
 
     vis.xAxisG.call(d3.axisBottom(vis.xScale));
     vis.yAxisG.call(d3.axisLeft(vis.yScale));
-
 
     /*
         WORKING ----------------------------------------------------------
     */
     // draw line
-    const lineSelectionworking = vis.svg.selectAll(".line-path-working")
+    const lineSelectionworking = vis.svg
+      .selectAll(".line-path-working")
       .data([vis.workingData]);
 
     lineSelectionworking.exit().remove();
 
-    const lineEnterworking = lineSelectionworking.enter()
+    const lineEnterworking = lineSelectionworking
+      .enter()
       .append("path")
       .attr("class", "line line-path");
 
-    lineEnterworking.merge(lineSelectionworking)
+    lineEnterworking
+      .merge(lineSelectionworking)
       .transition()
       .duration(1000)
       .attr("d", vis.lineGenerator)
       .attr("stroke", "blue")
-      .attr("fill", "none");  // add this line to remove the fill
+      .attr("fill", "none"); // add this line to remove the fill
 
     // add circles for each data point
-    const circles = vis.svg.selectAll(".dot")
-      .data(vis.workingData, d => d.month);
+    const circles = vis.svg
+      .selectAll(".dot")
+      .data(vis.workingData, (d) => d.month);
 
     circles.exit().remove();
 
-    circles.enter()
+    circles
+      .enter()
       .append("circle")
       .attr("class", "workingDot")
       .attr("r", 3)
       .merge(circles)
       .transition()
       .duration(1000)
-      .attr("cx", d => vis.xScale(d.month))
-      .attr("cy", d => vis.yScale(d.cortisol));
-
+      .attr("cx", (d) => vis.xScale(d.month))
+      .attr("cy", (d) => vis.yScale(d.cortisol));
 
     /*
       RESTING -------------------------------------------------------------
     */
-    const lineSelectionRest = vis.svg.selectAll(".line-path-rest")
+    const lineSelectionRest = vis.svg
+      .selectAll(".line-path-rest")
       .data([vis.restData]);
 
     lineSelectionRest.exit().remove();
 
-    const lineEnterRest = lineSelectionRest.enter()
+    const lineEnterRest = lineSelectionRest
+      .enter()
       .append("path")
       .attr("class", "line line-path");
 
-    lineEnterRest.merge(lineSelectionRest)
+    lineEnterRest
+      .merge(lineSelectionRest)
       .transition()
       .duration(1000)
       .attr("d", vis.lineGenerator)
       .attr("stroke", "brown")
-      .attr("fill", "none");  // add this line to remove the fill
+      .attr("fill", "none"); // add this line to remove the fill
 
     // add circles for each data point
-    const circlesRest = vis.svg.selectAll(".dot")
-      .data(vis.restData, d => d.month);
+    const circlesRest = vis.svg
+      .selectAll(".dot")
+      .data(vis.restData, (d) => d.month);
 
     circlesRest.exit().remove();
 
-    circlesRest.enter()
+    circlesRest
+      .enter()
       .append("circle")
       .attr("class", "restDot")
       .attr("r", 3)
       .merge(circlesRest)
       .transition()
       .duration(1000)
-      .attr("cx", d => vis.xScale(d.month))
-      .attr("cy", d => vis.yScale(d.cortisol));
-
+      .attr("cx", (d) => vis.xScale(d.month))
+      .attr("cy", (d) => vis.yScale(d.cortisol));
 
     /*
       Meditating -------------------------------------------------------------
     */
-    const lineSelectionMeditating = vis.svg.selectAll(".line-path-meditating")
+    const lineSelectionMeditating = vis.svg
+      .selectAll(".line-path-meditating")
       .data([vis.meditatingData]);
 
     lineSelectionMeditating.exit().remove();
 
-    const lineEnterMeditating = lineSelectionMeditating.enter()
+    const lineEnterMeditating = lineSelectionMeditating
+      .enter()
       .append("path")
       .attr("class", "line line-path");
 
-    lineEnterMeditating.merge(lineSelectionMeditating)
+    lineEnterMeditating
+      .merge(lineSelectionMeditating)
       .transition()
       .duration(1000)
       .attr("d", vis.lineGenerator)
       .attr("stroke", "green")
-      .attr("fill", "none");  // add this line to remove the fill
+      .attr("fill", "none"); // add this line to remove the fill
 
     // add circles for each data point
-    const circlesMeditating = vis.svg.selectAll(".dot")
-      .data(vis.meditatingData, d => d.month);
+    const circlesMeditating = vis.svg
+      .selectAll(".dot")
+      .data(vis.meditatingData, (d) => d.month);
 
     circlesMeditating.exit().remove();
 
-    circlesMeditating.enter()
+    circlesMeditating
+      .enter()
       .append("circle")
       .attr("class", "meditatingDot")
       .attr("r", 3)
       .merge(circlesMeditating)
       .transition()
       .duration(1000)
-      .attr("cx", d => vis.xScale(d.month))
-      .attr("cy", d => vis.yScale(d.cortisol));
-
+      .attr("cx", (d) => vis.xScale(d.month))
+      .attr("cy", (d) => vis.yScale(d.cortisol));
 
     // insert annotations
-    const annotationGroup = vis.svg.selectAll(".annotation-group")
+    const annotationGroup = vis.svg
+      .selectAll(".annotation-group")
       .data(vis.annotations);
 
     annotationGroup.exit().remove();
 
-    const annotationEnter = annotationGroup.enter()
+    const annotationEnter = annotationGroup
+      .enter()
       .append("g")
       .attr("class", "annotation-group");
 
-    annotationEnter.append("text")
+    annotationEnter
+      .append("text")
       .attr("class", "annotation")
       .attr("text-anchor", "start")
       .merge(annotationGroup.select("text"))
-      .attr("x", d => vis.xScale(d.month) + 2)
-      .attr("y", d => vis.yScale(d.cortisol) + 3)
-      .text(d => d.label);
-
+      .attr("x", (d) => vis.xScale(d.month) + 2)
+      .attr("y", (d) => vis.yScale(d.cortisol) + 3)
+      .text((d) => d.label);
 
     // add legend
     let legend = vis.svg.select(".legend");
@@ -225,14 +253,16 @@ class LineChart {
       const legendData = [
         { name: "Working", color: "blue" },
         { name: "Resting", color: "brown" },
-        { name: "Meditating", color: "green" }
+        { name: "Meditating", color: "green" },
       ];
 
-      legend = vis.svg.append("g")
+      legend = vis.svg
+        .append("g")
         .attr("class", "legend")
         .attr("transform", `translate(${vis.width - 100}, 10)`);
 
-      legend.selectAll("rect")
+      legend
+        .selectAll("rect")
         .data(legendData)
         .enter()
         .append("rect")
@@ -240,9 +270,10 @@ class LineChart {
         .attr("y", (d, i) => i * 20)
         .attr("width", 10)
         .attr("height", 10)
-        .attr("fill", d => d.color);
+        .attr("fill", (d) => d.color);
 
-      legend.selectAll("text")
+      legend
+        .selectAll("text")
         .data(legendData)
         .enter()
         .append("text")
@@ -250,7 +281,7 @@ class LineChart {
         .attr("y", (d, i) => i * 20 + 9)
         .attr("text-anchor", "start")
         .style("font-size", "12px")
-        .text(d => d.name);
+        .text((d) => d.name);
     }
   }
 }
---
+++ js/ScatterPlot2.js
@@ -0,0 +1,187 @@
+class ScatterPlot2 {
+  constructor(_config) {
+    this.config = {
+      parentElement: _config.parentElement,
+      width: _config.width || 900,
+      height: _config.height || 400,
+      margin: _config.margin || { top: 30, right: 30, bottom: 50, left: 60 },
+    };
+    this.data = _config.data;
+    this.initVis();
+  }
+
+  initVis() {
+    const vis = this;
+
+    vis.width =
+      document.getElementById(vis.config.parentElement).getBoundingClientRect()
+        .width -
+      vis.config.margin.left -
+      vis.config.margin.right;
+    vis.height =
+      document.getElementById(vis.config.parentElement).getBoundingClientRect()
+        .height -
+      vis.config.margin.top -
+      vis.config.margin.bottom;
+
+    // create svg
+    vis.svg = d3
+      .select("#" + vis.config.parentElement)
+      .append("svg")
+      .attr(
+        "width",
+        vis.config.width + vis.config.margin.left + vis.config.margin.right
+      )
+      .attr(
+        "height",
+        vis.config.height + vis.config.margin.top + vis.config.margin.bottom
+      )
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${vis.config.margin.left},${vis.config.margin.top})`
+      );
+
+    // scales
+    vis.xScale = d3.scaleLinear().range([0, vis.width]);
+
+    vis.yScale = d3.scaleLinear().range([vis.height, 0]);
+
+    // axis groups
+    vis.xAxisG = vis.svg
+      .append("g")
+      .attr("transform", `translate(0, ${vis.height})`);
+
+    vis.yAxisG = vis.svg.append("g");
+
+    // axis labels
+    vis.svg
+      .append("text")
+      .attr("class", "x-axis-label")
+      .attr("x", vis.width / 2)
+      .attr("y", vis.height + 35)
+      .attr("text-anchor", "middle")
+      .text("Height (cm)");
+
+    vis.svg
+      .append("text")
+      .attr("class", "y-axis-label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -40)
+      .attr("x", -vis.height / 2)
+      .attr("text-anchor", "middle")
+      .text("Weight (Kg)");
+
+    vis.wrangleData();
+  }
+
+  wrangleData() {
+    let vis = this;
+    console.log(vis.data);
+
+    vis.updateVis();
+  }
+
+  updateVis() {
+    const vis = this;
+
+    // update scales and axes
+    vis.xScale.domain([
+      d3.min(vis.data, (d) => d.height) - 10,
+      d3.max(vis.data, (d) => d.height) + 10,
+    ]);
+    vis.yScale.domain([
+      d3.min(vis.data, (d) => d.weight) - 10,
+      d3.max(vis.data, (d) => d.weight) + 10,
+    ]);
+
+    vis.xAxisG.call(d3.axisBottom(vis.xScale));
+    vis.yAxisG.call(d3.axisLeft(vis.yScale));
+
+    let bmis = [18.5, 25];
+    bmis.forEach((b) => {
+      let lineData = d3.range(
+        d3.min(vis.data, (d) => d.height) - 10,
+        d3.max(vis.data, (d) => d.height) + 10
+      );
+      lineData = lineData.map((d) => {
+        let w = b * (d / 100) * (d / 100);
+        return { h: d, w: w };
+      });
+      let line = d3
+        .line()
+        .x((d) => vis.xScale(d.h))
+        .y((d) => vis.yScale(d.w));
+      vis.svg
+        .append("path")
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("stroke", "blue")
+        .attr("d", line(lineData));
+
+      vis.svg
+        .append("text")
+        .attr("x", vis.xScale(lineData[lineData.length - 1].h))
+        .attr("y", vis.yScale(lineData[lineData.length - 1].w) - 10)
+        .attr("text-anchor", "middle")
+        .text(`BMI ${b}`);
+    });
+
+    // draw circles
+    const circles = vis.svg.selectAll("c").data(vis.data);
+
+    circles.exit().remove();
+
+    circles
+      .enter()
+      .append("circle")
+      .attr("class", "c")
+      .attr("r", (d) => d.rhr / 7)
+      .attr("fill", (d) => {
+        if (d.rhr < 75) return "#7BB662";
+        else if (d.rhr < 90) return "#FFD301";
+        else return "#D61F1F";
+      })
+      .attr("opacity", 0.6)
+      .attr("stroke", "black")
+      .attr("stroke-width", 1)
+      .merge(circles)
+      // .transition()
+      // .duration(1000)
+      .attr("cx", (d) => vis.xScale(d.height))
+      .attr("cy", (d) => vis.yScale(d.weight));
+
+    // add a legend for the resting heart rate colors
+    const legendData = [
+      { label: "Low Resting Heart Rate (<75)", color: "#7BB662" },
+      { label: "Moderate Resting Heart Rate (<90)", color: "#FFD301" },
+      { label: "High Resting Heart Rate (>=90)", color: "#D61F1F" },
+    ];
+
+    // remove any old legends
+    vis.svg.selectAll(".legend").remove();
+
+    // add the legends
+    const legend = vis.svg
+      .selectAll(".legend")
+      .data(legendData)
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", (d, i) => `translate(${50}, ${20 + i * 25})`);
+
+    legend
+      .append("rect")
+      .attr("x", 0)
+      .attr("width", 20)
+      .attr("height", 20)
+      .attr("fill", (d) => d.color);
+
+    legend
+      .append("text")
+      .attr("x", 30)
+      .attr("y", 15)
+      .style("font-size", "12px")
+      .text((d) => d.label);
+  }
+}
